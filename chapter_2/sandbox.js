d1b80e4f60d4c48e1fa9b642ba7885eee885b3dd
Sy Tu Nguyen <54698578+sytunguyen77@users.noreply.github.com>
2023-09-15 02:43:01 +0700

    changed

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,14 +1,14 @@
 // strings
-console.log('hello, world');
+console.log("hello, world");
 
-let email = 'mario@thenetninja.co.uk';
+let email = "mario@thenetninja.co.uk";
 console.log(email);
 
 // string concatenation
-let firstName = 'Brandon';
-let lastName = 'Sanderson';
+let firstName = "Brandon";
+let lastName = "Sanderson";
 
-let fullName = firstName + ' ' + lastName;
+let fullName = firstName + " " + lastName;
 
 console.log(fullName);
 
@@ -23,7 +23,5 @@ console.log(fullName.toUpperCase());
 let result = fullName.toLocaleLowerCase();
 console.log(result);
 
-let index = email.indexOf('@');
-console.log('index of the @ sign:', index);
-
-
+let index = email.indexOf("@");
+console.log("index of the @ sign:", index);
